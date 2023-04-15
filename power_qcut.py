ac7e346c447f45e4f2767e7c275ce03baf21da2b
mikolajzalewski <mikolajzalewskigm@gmail.com>
2023-04-15 10:06:28 +0200

    remove comments

--- power_qcut.py
+++ power_qcut.py
@@ -93,51 +93,6 @@ def qcut_selection(data: pd.DataFrame, y_col: str, cols_to_check: List[str], sco
     return_df = return_df.assign(min_max_ratio = lambda x: x.max_val/abs(x.min_val))
     return return_df.sort_values('mono')
 
-# def cut_iterations(col, data, y_col, score_function, n_bins):
-#     '''
-#     Function to calculate the monotonicity of a variable
-#     '''
-#     try:
-#         if isinstance(col, str):
-#             qcut_vals = data.groupby(pd.cut(data.eval(col), n_bins))[y_col].agg(score_function)
-#             ref_x = list(range(1, n_bins+1))
-#             temp_array = np.array([ref_x, [1]*n_bins])
-#             slope = sm.OLS(qcut_vals, temp_array.T).fit().params.x1
-#             mono = scipy.stats.spearmanr(ref_x, qcut_vals)
-#             n_features = len(col.split(' '))
-#             n_features = n_features - n_features//2
-#             return [col, n_features, slope, mono[0],
-#                                 qcut_vals.min(), qcut_vals.max(),
-#                                 qcut_vals[qcut_vals>0].sum(), qcut_vals[qcut_vals<0].sum()
-#                        ]
-#         if isinstance(col, list):
-#             print(col, 'is a list')
-#     except ValueError as errors:
-#         print(col, errors)
-#     except IndexError as errors:
-#         print(col, errors)
-#     return []
-
-# def cut_selection(data: pd.DataFrame, y_col: str, cols_to_check: List[str], score_function: Callable,
-#                   n_bins: int) -> pd.DataFrame:
-#     """ Returns dataframe with features and their parameters for monotonicity check, such as slope and spearman correlation, due to cut method.
-        
-#     Args:
-#         data: dataframe with data
-#         y_col: name of column with target variable
-#         cols_to_check: list of columns to check
-#         score_function: function to calculate score for each bin
-#         n_bins: number of bins to use
-#     Returns:
-#         dataframe with features and their parameters for monotonicity check
-#     """
-#     results = []
-#     results = joblib.Parallel(n_jobs=-1)(joblib.delayed(cut_iterations)(item, data, y_col, score_function, n_bins)
-#                                          for item in tqdm(cols_to_check))
-#     return_df = pd.DataFrame([r for r in results if len(r)], columns = ['feature', 'n_features', 'slope', 'mono', 'min_val', 'max_val', 'sum_pos', 'sum_neg'])
-#     return_df = return_df.assign(min_max_ratio = lambda x: x.max_val/abs(x.min_val))
-#     return return_df.sort_values('mono')
-
 
 def common_member(a_list, b_list):
     """ Returns common elements from two lists
@@ -194,40 +149,3 @@ def qcut_fold_validation(data_set: pd.DataFrame, k_number: int, y_col: str, cols
     return res
 
 
-# def cut_fold_validation(data_set: pd.DataFrame, k_number: int, y_col: str, cols_to_check: List[str], score_function: Callable,
-#                         n_bins: int, condition: str = 'abs(mono) > 0.8') -> pd.DataFrame:
-#     """Function is used to select features for monotonicity check. It splits data into k_number 
-#        of parts and checks monotonicity for each part. Then it check if common feature is monotonic on whole dataset and returns it. 
-#     Args:
-#         data_set: dataframe with data
-#         k_number: number of parts to split data
-#         y_col: name of column with target variable
-#         cols_to_check: list of columns to check
-#         score_function: function to calculate score for each bin
-#         n_bins: number of bins to be used
-#         condition: condition to select features
-#     Returns:
-#         dataframe with features and their parameters for monotonicity check
-#     """
-#     leng = int(data_set.shape[0] / k_number)
-#     results = cols_to_check
-#     for i in range(k_number):
-#         clear_output()
-#         print('Round ', i+1, '/', k_number)
-#         data_frame = data_set[leng * i: leng * (i+1)]
-#         res = cut_selection(data_frame,
-#                             y_col,
-#                             results,
-#                             score_function,
-#                             n_bins).query(condition)
-#         results = common_member(results, list(res.feature))
-#         if res.feature.shape[0] == 0:
-#             clear_output()
-#             print('Zero features :(')
-#             return pd.DataFrame([])
-#     clear_output()
-#     res = cut_selection(data_set,
-#                         y_col,
-#                         results,
-#                         score_function, n_bins).query(condition)
-#     return res
